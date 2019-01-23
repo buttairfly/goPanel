155bef984da665dd61c4dc7e9931f857f6f4ac17
Till Max Schwikal <schwikal@mailbox.org>
2019-01-23 22:32:24 +0100

    Move main go file

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if env GOOS=linux GOARCH=arm GOARM=5 go build ./src/main/gopanel.go ; then
+if env GOOS=linux GOARCH=arm GOARM=5 go build ./src/gopanel.go ; then
     if scp gopanel pi@ledpix:~/goPanel ; then
         exit 0
     elif scp gopanel pi@ledpix.fritz.box:~/goPanel ; then
