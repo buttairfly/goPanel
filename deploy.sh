d12b8ad18312f6f633b53fc2be2aea6883faee25
Till Max Schwikal <schwikal@mailbox.org>
2019-02-16 12:18:58 +0100

    Make deploy more verbose

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 if env GOOS=linux GOARCH=arm GOARM=5 go build ./src/gopanel.go ; then
+    echo "build  ./src/gopanel.go"
     if scp gopanel pi@ledpix:~/goPanel ; then
+        echo "deploy ./src/gopanel.go"
         exit 0
     elif scp gopanel pi@ledpix.fritz.box:~/goPanel ; then
+        echo "deploy ./src/gopanel.go"
         exit 0
     else
         echo "deploy failed" >&2
         exit 1
     fi
 else
-    echo "build failed" >&2
+    echo "build  failed" >&2
     exit 1
 fi
