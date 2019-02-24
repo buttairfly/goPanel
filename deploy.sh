a3f6eab676417d437bb95905825ce9f9fdb84b46
Till Max Schwikal <8588522+buttairfly@users.noreply.github.com>
2019-02-24 17:03:55 +0100

    Move config files on deployed host to config folder

--- deploy.sh
+++ deploy.sh
@@ -15,11 +15,11 @@ echo -e "${GREEN}${BINARY}${NC}: compiled at ${BLUE}${DATE}${NC} with version ${
 if env GOOS=linux GOARCH=arm GOARM=5 go build -ldflags "-X main.compileDate=${DATE} -X main.versionTag=${VERSION}" -o ${BINARY} ./src ; then
     echo "build  ${BINARY}"
     if scp ${BINARY} pi@ledpix:~/goPanel ; then
-        scp ./config/* pi@ledpix:~/
+        scp -r ./config/ pi@ledpix:~/config
         echo "deploy ${BINARY}"
         exit 0
     elif scp ${BINARY} pi@ledpix.fritz.box:~/goPanel ; then
-        scp ./config/* pi@ledpix.fritz.box:~/config
+        scp -r ./config/ pi@ledpix.fritz.box:~/config
         echo "deploy ${BINARY}"
         exit 0
     else
