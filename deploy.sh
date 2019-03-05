ebfc9de4ed75fbbd1db73503566d7712bffe2e74
Till Max Schwikal <8588522+buttairfly@users.noreply.github.com>
2019-03-05 11:21:08 +0100

    Update deploy to rsync

--- deploy.sh
+++ deploy.sh
@@ -1,32 +1,43 @@
 #!/bin/bash
 
+BINARY="gopanel"
+VERSION=`git describe --always --dirty`
+DATE=`date -u +%FT%T%z`
+ENV='env GOOS=linux GOARCH=arm GOARM=5'
+
 # color codes
+RED="\033[0;31m"
 GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 LIGHT_BLUE='\033[1;34m'
 NC='\033[0m' # No Color
 
-BINARY=gopanel
-VERSION=`git describe --always --dirty`
-DATE=`date -u +%FT%T%z`
+
+# commands
+function COPY {
+  rsync -acE --progress $1 $2
+}
+function BUILD {
+  return $(`${ENV} go build -ldflags "-X main.compileDate=${DATE} -X main.versionTag=${VERSION}" -o ${BINARY} ./src`)
+}
 
 echo -e "${GREEN}${BINARY}${NC}: compiled at ${BLUE}${DATE}${NC} with version ${LIGHT_BLUE}${VERSION}${NC}"
 
-if env GOOS=linux GOARCH=arm GOARM=5 go build -ldflags "-X main.compileDate=${DATE} -X main.versionTag=${VERSION}" -o ${BINARY} ./src ; then
-    echo "build  ${BINARY}"
-    if scp ${BINARY} pi@ledpix:~/goPanel ; then
-        scp -rp ./config/ pi@ledpix:~/config
-        echo "deploy ${BINARY}"
+if BUILD; then
+    echo -e "build  ${GREEN}${BINARY}${NC}"
+    if COPY ./${BINARY} pi@ledpix:~/${BINARY} ; then
+        COPY ./config/ pi@ledpix:~/config
+        echo -e "deploy ${GREEN}${BINARY}${NC}"
         exit 0
-    elif scp ${BINARY} pi@ledpix.fritz.box:~/goPanel ; then
-        scp -rp ./config/ pi@ledpix.fritz.box:~/config
-        echo "deploy ${BINARY}"
+    elif COPY ./${BINARY} pi@ledpix.fritz.box:~/${BINARY} ; then
+        COPY ./config/ pi@ledpix.fritz.box:~/config
+        echo -e "deploy ${GREEN}${BINARY}${NC}"
         exit 0
     else
-        echo "deploy failed" >&2
+        echo -e "deploy ${RED}failed${NC}"
         exit 1
     fi
 else
-    echo "build  failed" >&2
+    echo -e "build  ${RED}failed${NC}"
     exit 1
 fi
