69723b6ce9c3e8aecbb1806bd3f89036e6cffa1c
Till Max Schwikal <8588522+buttairfly@users.noreply.github.com>
2019-03-08 15:32:08 +0100

    Add time sleep

--- src/device/serialCommands.go
+++ src/device/serialCommands.go
@@ -97,6 +97,7 @@ func (s *serialDevice) printLatches(wg *sync.WaitGroup) {
 		default:
 			timeDiff := start.Sub(time.Now()) / time.Second
 			log.Printf("Latched frames: %f/s last diff: %v", float64(s.latched)/float64(timeDiff), timeDiff)
+			time.Sleep(30 * time.Second)
 		}
 	}
 }
