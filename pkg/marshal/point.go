a2407d00dd25555a49e4b42bdf4fe53baaf29741
Till Max Schwikal <8588522+buttairfly@users.noreply.github.com>
2020-05-28 18:42:08 +0200

    add doc

--- pkg/marshal/point.go
+++ pkg/marshal/point.go
@@ -2,15 +2,18 @@ package marshal
 
 import "image"
 
+// Point is the marshalable version of image.Point
 type Point struct {
 	X int `json:"px" yaml:"px"`
 	Y int `json:"py" yaml:"py"`
 }
 
+// ToImagePoint converts a marshalable Point to image.Point
 func (p *Point) ToImagePoint() image.Point {
 	return image.Point{p.X, p.Y}
 }
 
+// FromImagePoint converts a image.Point to the marshal version
 func FromImagePoint(ip image.Point) Point {
 	return Point{ip.X, ip.Y}
 }
--- pkg/marshal/rectangle.go
+++ pkg/marshal/rectangle.go
@@ -2,15 +2,18 @@ package marshal
 
 import "image"
 
+// Rectangle is the marshalable version of image.Rectangle
 type Rectangle struct {
 	Min Point `json:"min" yaml:"min"`
 	Max Point `json:"max" yaml:"max"`
 }
 
+// ToImageRectangle converts a marshalable Rectangle to image.Rectangle
 func (b *Rectangle) ToImageRectangle() image.Rectangle {
 	return image.Rectangle{b.Min.ToImagePoint(), b.Max.ToImagePoint()}
 }
 
+// FromImageRectangle converts a image.Rectangle to the marshal version
 func FromImageRectangle(ib image.Rectangle) Rectangle {
 	return Rectangle{FromImagePoint(ib.Min), FromImagePoint(ib.Max)}
 }
