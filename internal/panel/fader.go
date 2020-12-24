5255ee70df6965e734767586fa1e22998d06b7f8
Till Max Schwikal <8588522+buttairfly@users.noreply.github.com>
2020-12-24 03:57:46 +0100

    Move fader functionality

---
+++ internal/panel/fader.go
@@ -0,0 +1,21 @@
+package panel
+
+import (
+	"errors"
+
+	"github.com/buttairfly/goPanel/pkg/fader"
+)
+
+// GetFaders returns the panel faders
+func (me *Panel) GetFaders() map[string]fader.Fader {
+	return me.faders
+}
+
+// GetFaderByID returns the panel fader by id
+func (me *Panel) GetFaderByID(id string) (*fader.Fader, error) {
+	fader, exists := me.faders[id]
+	if exists {
+		return &fader, nil
+	}
+	return nil, errors.New("not found")
+}
